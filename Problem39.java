72840cd350ed29ab1841e174d18fa668afb8a6ca
Sahil Vishwakarma <sahilvishwa2108@gmail.com>
2024-02-09 00:13:19 +0530

    Problem 40 solved

--- Problem39.java
+++ Problem39.java
@@ -20,4 +20,30 @@ Explanation: 13 = 4 + 9.
 
 Constraints:
 
-1 <= n <= 104 */
+1 <= n <= 10^4 */
+
+
+class Solution {
+    public int numSquares(int n) {
+        // Create an array to store the minimum number of perfect squares required to sum up to each index
+        int[] dp = new int[n + 1];
+        
+        // Fill the dp array with max possible value
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        
+        // Base case: 0 requires 0 perfect squares
+        dp[0] = 0;
+        
+        // Iterate from 1 to n
+        for (int i = 1; i <= n; i++) {
+            // Try every possible perfect square less than or equal to i
+            for (int j = 1; j * j <= i; j++) {
+                // Update dp[i] if using the current perfect square reduces the number of squares required
+                dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
+            }
+        }
+        
+        // The result is stored at dp[n]
+        return dp[n];
+    }
+}
