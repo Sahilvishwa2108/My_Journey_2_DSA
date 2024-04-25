73397bb2c235dc12af29a9beb023819877328736
Sahil Vishwakarma <sahilvishwa2108@gmail.com>
2024-04-25 23:38:35 +0530

    edit

--- Problem112.js
+++ Problem112.js
@@ -1,52 +1,30 @@
-/* 2634. Filter Elements from Array
-Given an integer array arr and a filtering function fn, return a filtered array filteredArr.
-
-The fn function takes one or two arguments:
-
-arr[i] - number from the arr
-i - index of arr[i]
-filteredArr should only contain the elements from the arr for which the expression fn(arr[i], i) evaluates to a truthy value. A truthy value is a value where Boolean(value) returns true.
-
-Please solve it without the built-in Array.filter method.
+/* 2621. Sleep
+Given a positive integer millis, write an asynchronous function that sleeps for millis milliseconds. It can resolve any value.
 
  
 
 Example 1:
 
-Input: arr = [0,10,20,30], fn = function greaterThan10(n) { return n > 10; }
-Output: [20,30]
-Explanation:
-const newArray = filter(arr, fn); // [20, 30]
-The function filters out values that are not greater than 10
+Input: millis = 100
+Output: 100
+Explanation: It should return a promise that resolves after 100ms.
+let t = Date.now();
+sleep(100).then(() => {
+  console.log(Date.now() - t); // 100
+});
 Example 2:
 
-Input: arr = [1,2,3], fn = function firstIndex(n, i) { return i === 0; }
-Output: [1]
-Explanation:
-fn can also accept the index of each element
-In this case, the function removes elements not at index 0
-Example 3:
-
-Input: arr = [-2,-1,0,1,2], fn = function plusOne(n) { return n + 1 }
-Output: [-2,0,1,2]
-Explanation:
-Falsey values such as 0 should be filtered out
+Input: millis = 200
+Output: 200
+Explanation: It should return a promise that resolves after 200ms.
  
 
 Constraints:
 
-0 <= arr.length <= 1000
--10^9 <= arr[i] <= 10^9
+1 <= millis <= 1000
+
 
 solution is - */
-
-var filter = function(arr, fn) {
-    const filteredArr = [];
-
-    for(let i = 0; i < arr.length; i++) {
-        if (fn(arr[i], i)) {
-            filteredArr.push(arr[i])
-        } 
-    }
-    return filteredArr;
-};
+async function sleep(millis = 0) {
+return new Promise(resolve => setTimeout(resolve, millis));
+}
