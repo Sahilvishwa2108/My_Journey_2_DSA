378c2c775eec4db9b0ae54a92052c6a24b93a14a
sahilvishwa2108 <sahilvishwa2108@gmail.com>
2024-06-30 23:45:37 +0530

    solved problem 174

---
+++ Problem174.cpp
@@ -0,0 +1,43 @@
+/* 69. Sqrt(x)
+Given a non-negative integer x, return the square root of x rounded down to the nearest integer. The returned integer should be non-negative as well.
+
+You must not use any built-in exponent function or operator.
+
+For example, do not use pow(x, 0.5) in c++ or x ** 0.5 in python.
+ 
+
+Example 1:
+
+Input: x = 4
+Output: 2
+Explanation: The square root of 4 is 2, so we return 2.
+Example 2:
+
+Input: x = 8
+Output: 2
+Explanation: The square root of 8 is 2.82842..., and since we round it down to the nearest integer, 2 is returned.
+ 
+
+Constraints:
+
+0 <= x <= 231 - 1
+ */
+
+class Solution {
+ public:
+  int mySqrt(int x) {
+    unsigned l = 1;
+    unsigned r = x + 1u;
+
+    while (l < r) {
+      const unsigned m = (l + r) / 2;
+      if (m > x / m)
+        r = m;
+      else
+        l = m + 1;
+    }
+
+    // l := the minimum number s.t. l * l > x
+    return l - 1;
+  }
+};
